618ba1b72350675471213f6643324106a09f65a3
natukin1978 <natukin1978@hotmail.com>
2025-02-05 23:42:17 +0900

    Added new parameter "noisy"
    Planned to be used to capture the situation roughly.

--- youtube_message_helper.py
+++ youtube_message_helper.py
@@ -14,6 +14,7 @@ def create_message_json(item=None) -> dict[str, any]:
         "content": None,
         "isFirst": False,
         "isFirstOnStream": None,  # すぐ下で設定する
+        "noisy": False,
         "additionalRequests": None,
     }
     if item:
